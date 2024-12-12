9686a3c57df93a876af473871d3626e555d3c06a
tylersemel <tylersemel@gmail.com>
2024-12-12 16:30:48 -0500

    Create basic styling for book display

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        if (this.read) {
+        if (!this.read) {
             return `${title} by ${author}, ${pages} pages, not read yet.`;
         }
         return `${title} by ${author}, ${pages} pages, has been read.`
-    }
+    };
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
+
+addBookToLibrary("The Hobbit", "Guy", 295, true);
+console.log(myLibrary[0].info());
