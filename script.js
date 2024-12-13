554053b32c5d7d85025eec4172c759d82e5ec2ea
tylersemel <tylersemel@gmail.com>
2024-12-13 18:13:21 -0500

    Restyle book covers

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const addBookForm = document.querySelector('.add-book-form');
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, index) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.index = index;
     this.info = function() {
         if (!this.read) {
             return `${title} by ${author}, ${pages} pages, not read yet.`;
@@ -19,66 +20,90 @@ function Book(title, author, pages, read) {
     };
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, read, index) {
+    let newBook = new Book(title, author, pages, read, index);
     myLibrary.push(newBook);
 }
+
+// function createBookCard(book) {
+//     let cardArticle = document.createElement('article');
+//     cardArticle.classList.add('card');
+
+//     //create top of book in article
+//     let bookTopDiv = document.createElement('div');
+//     bookTopDiv.classList.add('book-top');
+
+//     let readDiv = document.createElement('div');
+//     readDiv.classList.add('read');
+
+//     if (book.read) {
+//         readDiv.textContent = '✓';
+//     }
+//     else {
+//         readDiv.textContent = '!';
+//     }
+
+//     let pagesDiv = document.createElement('div');
+//     pagesDiv.classList.add('pages');
+//     pagesDiv.textContent = book.pages.toString() + ' pp.';
+
+//     bookTopDiv.appendChild(readDiv);
+//     bookTopDiv.appendChild(pagesDiv);
+
+//     //create bottom of book
+//     let bookBottomHeader = document.createElement('header');
+//     bookBottomHeader.classList.add('book-bottom');
+
+//     let titleH3 = document.createElement('h3');
+//     titleH3.textContent = book.title;
+
+//     let authorSpan = document.createElement('span');
+//     authorSpan.textContent = book.author;
+
+//     bookBottomHeader.appendChild(titleH3);
+//     bookBottomHeader.appendChild(authorSpan);
+
+//     //now append top and bottom to article
+//     cardArticle.appendChild(bookTopDiv);
+//     cardArticle.appendChild(bookBottomHeader);
+
+//     //attach article to books section
+//     booksSection.appendChild(cardArticle);
+// }
 
 function createBookCard(book) {
     let cardArticle = document.createElement('article');
     cardArticle.classList.add('card');
 
+    let removeDiv = document.createElement('div');
+    removeDiv.classList.add('remove');
+    let removeBtn = document.createElement('button');
+    removeBtn.textContent = 'X';
+    removeDiv.appendChild(removeBtn);
+
     //create top of book in article
-    let bookTopDiv = document.createElement('div');
-    bookTopDiv.classList.add('book-top');
+    let bookInfoDiv = document.createElement('div');
+    bookInfoDiv.classList.add('book-info');
 
-    let readDiv = document.createElement('div');
-    readDiv.classList.add('read');
+    bookInfoDiv.textContent = book.info();
 
-    if (book.read) {
-        readDiv.textContent = '✓';
-    }
-    else {
-        readDiv.textContent = '!';
-    }
-
-    let pagesDiv = document.createElement('div');
-    pagesDiv.classList.add('pages');
-    pagesDiv.textContent = book.pages.toString() + ' pp.';
-
-    bookTopDiv.appendChild(readDiv);
-    bookTopDiv.appendChild(pagesDiv);
-
-    //create bottom of book
-    let bookBottomHeader = document.createElement('header');
-    bookBottomHeader.classList.add('book-bottom');
-
-    let titleH3 = document.createElement('h3');
-    titleH3.textContent = book.title;
-
-    let authorSpan = document.createElement('span');
-    authorSpan.textContent = book.author;
-
-    bookBottomHeader.appendChild(titleH3);
-    bookBottomHeader.appendChild(authorSpan);
-
-    //now append top and bottom to article
-    cardArticle.appendChild(bookTopDiv);
-    cardArticle.appendChild(bookBottomHeader);
-
-    //attach article to books section
+    cardArticle.appendChild(removeDiv);
+    cardArticle.appendChild(bookInfoDiv);
     booksSection.appendChild(cardArticle);
 }
 
-//gonna have a button that you click to show the form
-//it'll show up in the center and then the user fills in the details
-//separate button to add the book
-
 function populateBookCards() {
+    addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, true));
+    addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, true));
+    addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, true));
+    addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, false));
+    addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, true));
 
     for (let book of myLibrary) {
         createBookCard(book.title, book.author, book.pages, book.read);
     }
+
+    console.log(myLibrary[3]);
 }
 
 showAddDialogBtn.addEventListener('click', () => {
@@ -103,9 +128,11 @@ addBookForm.addEventListener('submit', (e) => {
         readValue = false;
     }
 
-    addBookToLibrary(titleValue.toString(), authorValue.toString(), pagesValue.toString(), readValue);
+    addBookToLibrary(titleValue.toString(), authorValue.toString(), pagesValue.toString(), readValue, myLibrary.length);
     createBookCard(myLibrary[myLibrary.length - 1]);
 
     addBookForm.reset();
     addDialog.close();
 });
+
+populateBookCards();
